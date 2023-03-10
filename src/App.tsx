b18e5f3a4a928eafa090ebe868b855197b2f8752
Hannes Kiehn <HannesKiehn@hotmail.de>
2023-03-11 00:16:06 +0100

    add seufz counter and woodstock

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import cloud from "./assets/cloud.jpg";
+import "./App.css";
+import Roman from "react-roman";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={cloud} className="woodstock" alt="woodstock on cloud" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Seufz Counter</h1>
+      <div className="roman">
+        <Roman>{count}</Roman>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="counter">
+        <button onClick={() => setCount((count) => count + 1)}>*seufz*</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
